58e318d7dd3753d95d1b5e24f30dc5f9a9962a76
asminok99 <ychildfr@bk.ru>
2023-11-09 15:19:54 +0600

    tuples.py home work

---
+++ tuples.py
@@ -0,0 +1,100 @@
+# person1 = ("yasemin","14.09.07","london")
+# person2 = ("Aziz","05.08.07","China")
+# person3 = ("Akniet","10.07.07","Africa")
+
+# people = [person1, person2, person3]
+
+# print(person1[0])
+# print(person2[1])
+# print(person3[2])
+
+# print(people[1][2])
+
+# print(len(people[0]))
+
+# 1
+# cities = ("New York", "Miami", "San Francisco")
+# cities = list(cities)
+# cities[1] = "lost Angles"
+# cities = tuple(cities)
+# print(cities)
+
+# 2
+# cities = ("New York", "Miami", "San Francisco")
+# cities = list(cities)
+# cities.insert(1, "Boston")
+
+# 3
+# cities = ("New York", "Miami", "San Francisco")
+# if "Denver" in cities:
+#    print("not")
+# else:
+#    cities = list(cities)
+#    cities.append = ("Denver")
+#    cities = tuple(cities)
+#    print(cities)
+
+# 4
+# cities = ("New York", "Miami", "San Francisco")
+# if "denver" in cities:
+#     print("legts go to denver")
+
+
+
+# home task
+# 1
+# sis = ()
+# 2
+# sis = ("Tana", "Anna", "Simba","aknetik")
+# 3
+# sis = ("Tana", "Anna", "Simba","aknetik")
+# print(len(sis))
+# 4
+# sis = ("Tana", "Anna", "Simba","aknetik")
+# sis = list(sis)
+# sis.append = ("Mamy", "Dady")
+# sis = tuple(sis)
+# print(sis)
+
+# 5
+# sis = ("Tana", "Anna", "Simba","aknetik")
+# family = list(sis)
+# family.append("Mamy")
+# family.append("Dady")
+# family = tuple(family)
+# print(family)
+
+# №1
+# family_members = ("islam", "aziz", "Tana", "Anna", "Simba","aknetik")
+# brother1 , brother2 , sister1 , sister2 , parent1 , parent2 = family_members
+# print(brother1)
+# print(brother2)
+# print(sister1)
+# print(sister2)
+# print(parent1)
+# print(parent2)
+# 2
+
+# fruts = ("apple", "banana", "pinapple", "grape", "cherry")
+# vegetables = ("tomato", "potato", "lemon", "onion", "egg")
+# animals = ("beef", "veal", "pork", "mutton", "venison")
+# food_stuff_tp = fruts + vegetables + animals
+# print(food_stuff_tp)
+
+# food_stuff_tp = ('apple', 'banana', 'pinapple', 'grape', 'cherry', 'tomato', 'potato', 'lemon', 'onion', 'egg', 'beef', 'veal', 'pork', 'mutton', 'venison')
+# food_stuff_lt = list(food_stuff_tp)
+# print(food_stuff_lt[0:3])
+# print(food_stuff_lt[6:])
+# #6
+# food_stuff_tp = (('apple', 'banana', 'pinapple', 'grape', 'cherry', 'tomato', 'potato', 'lemon', 'onion', 'egg', 'beef', 'veal', 'pork', 'mutton', 'venison'))
+# del(food_stuff_tp)
+# #7
+# nordic_countries = ("finland", "denmark", "norway", "swedwn", "iseland")
+# if 'Estonia' in nordic_countries:
+#     print('it is in nordic_countries')
+# else:
+#     print('it isn\'t in nordic_contries')
+
+# nordic_countries = ("finland", "denmark", "norway", "swedwn", "iseland")
+# if "Iceland" in nordic_countries:
+#     print('it is in nordic_countries')
